a6954a14a865044610974be3d3a9384a20bfcec2
Muhammad-Talha103 <talhagp908@gmail.com>
2025-05-27 23:43:53 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import path from "path";
+import webpack from "webpack";
+import dotenv from "dotenv";
+
+// Load and parse the environment variables
+const { parsed: myEnv } = dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  webpack(config) {
+    if (myEnv) {
+      config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
